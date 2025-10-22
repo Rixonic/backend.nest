caa3b8992809a2027b2d74e4259e188ef592536a
Rixonic <franco.j.cejas@gmail.com>
2025-10-22 13:54:30 +0000

    correccion de plc service

--- src/services/plc/plc.service.ts
+++ src/services/plc/plc.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Alarms } from 'src/plc/plc.entity';
+import { Not, Like } from 'typeorm';
 
 @Injectable()
 export class PLCService {
@@ -11,9 +12,17 @@ export class PLCService {
   ) { }
 
   async findAll(): Promise<Alarms[]> {
-    return this.alarmsRepository.find({
+    const response = await this.alarmsRepository.find({
+      where: {
+        source: Not(Like('PLC_Temperatura%'))
+      },
       order: { e3TimeStamp: 'DESC' },
     });
+    //console.log(response[0])
+
+    return response
   }
+
+
   
 }
